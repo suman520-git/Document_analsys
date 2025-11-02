176f201daa28efa320a687ce757e661e81e1b78f
suman520-git <svemula520@gmail.com>
2025-11-02 15:21:34 +0530

    code ready for deployment

--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -1,3 +1,4 @@
+
 import os
 import logging
 from datetime import datetime
@@ -46,8 +47,8 @@ class CustomLogger:
         return structlog.get_logger(logger_name)
 
 
-# --- Usage Example ---
-if __name__ == "__main__":
-    logger = CustomLogger().get_logger(__file__)
-    logger.info("User uploaded a file", user_id=123, filename="report.pdf")
-    logger.error("Failed to process PDF", error="File not found", user_id=123)
+# # --- Usage Example ---
+# if __name__ == "__main__":
+#     logger = CustomLogger().get_logger(__file__)
+#     logger.info("User uploaded a file", user_id=123, filename="report.pdf")
+#     logger.error("Failed to process PDF", error="File not found", user_id=123)
